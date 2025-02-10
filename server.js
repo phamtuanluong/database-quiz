29800c182d09bc6403cdb5df7969981c0b7da2e2
Tuan Luong <tuanluongkt1907@gmail.com>
2025-02-11 00:02:14 +0700

    Database quiz

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("database.json"); // Đảm bảo `db.json` có trong thư mục
+const router = jsonServer.router("/database/database.json"); // Đảm bảo `db.json` có trong thư mục
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
