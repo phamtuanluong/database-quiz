b55c891e689a308c1f6c47c2bda3239f0e141565
Tuan Luong <tuanluongkt1907@gmail.com>
2025-02-11 00:07:41 +0700

    Database quiz

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("/database/database.json"); // Đảm bảo `db.json` có trong thư mục
+const router = jsonServer.router(path.join(__dirname, "database.json")); // Đường dẫn tuyệt đối
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
